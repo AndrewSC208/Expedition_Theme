f4087b7d49e63adff14da12011a70fb5c697aef5
unknown <Ameiling@ameiling2-w7.luthresearch.net>
2016-12-12 16:29:04 -0800

    Added slideout.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('\'Allo \'Allo!');
+// setup slideout
+var slideout = new Slideout({
+  'panel': document.getElementById('panel'),
+  'menu': document.getElementById('menu'),
+  'padding': 256,
+  'tolerance': 70
+});
