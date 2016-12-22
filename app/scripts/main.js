5857b6f3aeb0d9c5c816b4295f8a40bc5061dd5a
AndrewSC208 <meilingandrew@gmail.com>
2016-12-21 17:25:23 -0800

    Cleaned up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,9 @@
-// setup slideout
+// setup slideout (move to seprate file, and "import" in main)
 var slideout = new Slideout({
   'panel': document.getElementById('panel'),
   'menu': document.getElementById('menu'),
   'padding': 256,
   'tolerance': 70
 });
+
+
