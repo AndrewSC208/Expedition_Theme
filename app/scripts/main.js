35517b88cd66273c4a272285fec0880e7b62df75
AndrewSC208 <meilingandrew@gmail.com>
2017-01-05 14:15:24 -0800

    Main marketing cards updated FINAL

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,6 @@
+// figure out how to import this class
+//import * from './sideMenu';
+
 // not sure I like this, but I will look into how I can turn this into a class or something, maybe even drop the dependency on jquery
 $(".menu-btn").on('click', (el) => {
 	$('.menu-btn').toggleClass('open');
---
+++ app/scripts/sideMenu.js
@@ -0,0 +1,18 @@
+// sideMenu class
+//  - This will attach event listeners to menu buttons
+//  - Create methods for openinig and closing
+
+export class sideMenu {
+	constructor() {
+
+	}
+
+	open () {
+
+	}
+
+	close() {
+
+	}
+
+}
